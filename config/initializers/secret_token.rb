f06a89df7e32af385b13d095286efb61e899359f
Caleb <durante@wisc.edu>
2014-03-08 15:20:50 -0600

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HermesWeb::Application.config.secret_key_base = '67c0d5ca1acec171a8672f37dd8d552c5d776281bd50437e558460ac0ceb3146db6ee7fa6cbc2c02e21630682caeb716b79bac18249400bc9311c082190d45c7'
