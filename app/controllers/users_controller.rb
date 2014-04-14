79f394763dfea4e7edabcde9e6f569445560a895
Caleb <durante@wisc.edu>
2014-04-14 14:33:13 -0500

    Added JSON response to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    respond_to do |format|
+      format.html show.html.erb
+      format.xml { render :xml=> @user}
+      format.json {render:json => @user}
+    end
   end
   def new
     @user = User.new
