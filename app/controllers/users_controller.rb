0f576efa0ef7905d4f439403cb1db67dcc232af2
Caleb <durante@wisc.edu>
2014-04-14 14:58:35 -0500

    updated the HTML response in Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,18 @@
 class UsersController < ApplicationController
+ 
   def show
     @user = User.find(params[:id])
-    respond_to do |format|
-      format.html show.html.erb
-      format.xml { render :xml=> @user}
-      format.json {render:json => @user}
+       respond_to do |format|
+      format.html
+      format.xml { render :xml=> @user }
+      format.json { render :json => @user }
     end
   end
+
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
